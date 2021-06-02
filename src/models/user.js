2d204e5b896bd02a615cc95a779540877e9a6984
Johann Molano <jjdmolano@gmail.com>
2021-06-02 18:23:21 +0800

    Changed mongodb and jwt secret to env variables

--- src/models/user.js
+++ src/models/user.js
@@ -58,7 +58,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = async function () {
-	const token = jwt.sign({ _id: this._id.toString() }, 'codingbootcamp');
+	const token = jwt.sign({ _id: this._id.toString() }, process.env.JWT_SECRET);
 
 	this.tokens = this.tokens.concat({ token });
 	await this.save();
