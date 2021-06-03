335f9ae769e5d070fcb11710a6eca5a5cce7a1ca
Johann Molano <jjdmolano@gmail.com>
2021-06-03 14:29:29 +0800

    Added basic test notes

--- src/math.js
+++ src/math.js
@@ -1,8 +1,5 @@
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent
-    return total + tip
-}
+const calculateTip = (total, tipPercent = .25) => total + (tipPercent * total);
 
 module.exports = {
-    calculateTip
-}
+	calculateTip
+};
