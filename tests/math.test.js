0c95d59e1b4fd6c96c636c51d23ce5468e144fd2
Johann Molano <jjdmolano@gmail.com>
2021-06-03 12:09:16 +0800

    Tried to run test on personal laptop and tests worked.

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,3 +1,9 @@
-test('Hello!', () => {
+const { calculateTip } = require('../src/math')
 
+test('Test if calc tip works', () => {
+    const total = calculateTip(10, .3)
+
+    if (total !== 13) {
+        throw new Error(`Total tip should be 13. Got ${total}`)
+    }
 })
